4f8b4b7d93ea29b68bc7d5bec061d123b193fca3
Shreesh Kulkarni <sgkul2000@gmail.com>
2021-02-19 12:05:10 +0530

    Feat: Added favicon and cleared up the codebase

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/sgkul2000/mail-for-cause/pkg/db"
 	"github.com/sgkul2000/mail-for-cause/pkg/middlewares"
 )
 
@@ -31,138 +30,12 @@ func main() {
 	e.HTTPErrorHandler = middlewares.EchoErrorHandler
 	e.Use(middleware.Recover())
 
-	// e.Validator = &types.CustomValidator{Validator: validator.New()}
 	e.Static("/static", "views/static")
 	Register(e)
-	e.GET("/", func(c echo.Context) error {
-		// return c.Render(http.StatusOK, "base", struct {
-		// 	Page string
-		// }{
-		// 	Page: "index",
-		// })
 
-		causes, err := db.GetCauses()
-		if err != nil {
-			return err
-		}
-
-		tmpl, err := template.New("Main").Funcs(template.FuncMap{
-			"trim": func(description string) string {
-				if len(description) > 50 {
-					return description[:50] + " ..."
-				}
-				return description
-			},
-		}).ParseGlob("views/*.html")
-		if err != nil {
-			return err
-		}
-		return tmpl.ExecuteTemplate(c.Response().Writer, "base", struct {
-			Page string
-			Data interface{}
-		}{
-			Page: "index",
-			Data: causes,
-		})
-	})
-	e.GET("/signup", func(c echo.Context) error {
-		// return c.Render(http.StatusOK, "base", struct {
-		// 	Page string
-		// }{
-		// 	Page: "index",
-		// })
-		tmpl, err := template.New("Main").Funcs(template.FuncMap{
-			"trim": func(description string) string {
-				if len(description) > 50 {
-					return description[:50] + " ..."
-				}
-				return description
-			},
-		}).ParseGlob("views/*.html")
-		if err != nil {
-			return err
-		}
-		return tmpl.ExecuteTemplate(c.Response().Writer, "base", struct {
-			Page string
-		}{
-			Page: "signup",
-		})
-	})
-	e.GET("/login", func(c echo.Context) error {
-		// return c.Render(http.StatusOK, "base", struct {
-		// 	Page string
-		// }{
-		// 	Page: "index",
-		// })
-		tmpl, err := template.New("Main").Funcs(template.FuncMap{
-			"trim": func(description string) string {
-				if len(description) > 50 {
-					return description[:50] + " ..."
-				}
-				return description
-			},
-		}).ParseGlob("views/*.html")
-		if err != nil {
-			return err
-		}
-		return tmpl.ExecuteTemplate(c.Response().Writer, "base", struct {
-			Page string
-		}{
-			Page: "login",
-		})
-	})
-	e.GET("/create", func(c echo.Context) error {
-		// return c.Render(http.StatusOK, "base", struct {
-		// 	Page string
-		// }{
-		// 	Page: "index",
-		// })
-		tmpl, err := template.New("Main").Funcs(template.FuncMap{
-			"trim": func(description string) string {
-				if len(description) > 50 {
-					return description[:50] + " ..."
-				}
-				return description
-			},
-		}).ParseGlob("views/*.html")
-		if err != nil {
-			return err
-		}
-		return tmpl.ExecuteTemplate(c.Response().Writer, "base", struct {
-			Page string
-		}{
-			Page: "create",
-		})
-	})
-	e.GET("/cause/:name", func(c echo.Context) error {
-		cause, err := db.GetCause(c.Param("name"))
-		if err != nil {
-			return err
-		}
-		// return c.Render(http.StatusOK, "base", struct {
-		// 	Page string
-		// }{
-		// 	Page: "index",
-		// })
-		tmpl, err := template.New("Main").Funcs(template.FuncMap{
-			"trim": func(description string) string {
-				if len(description) > 50 {
-					return description[:50] + " ..."
-				}
-				return description
-			},
-		}).ParseGlob("views/*.html")
-		if err != nil {
-			return err
-		}
-		fmt.Println(cause)
-		return tmpl.ExecuteTemplate(c.Response().Writer, "base", struct {
-			Page string
-			Data interface{}
-		}{
-			Page: "send",
-			Data: cause,
-		})
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "1323"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
 }
--- cmd/register.go
+++ cmd/register.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/sgkul2000/mail-for-cause/pkg/db"
 	"github.com/sgkul2000/mail-for-cause/pkg/handler"
 )
 
@@ -20,4 +22,53 @@ func Register(e *echo.Echo) {
 	e.GET("/api/cause", handler.GetCause, middleware.JWT([]byte(os.Getenv("SECRET"))))
 	e.POST("/api/cause", handler.CreateCause, middleware.JWT([]byte(os.Getenv("SECRET"))))
 	e.PUT("/api/cause", handler.CreateCause, middleware.JWT([]byte(os.Getenv("SECRET"))))
+
+	// Views
+	e.GET("/", func(c echo.Context) error {
+		causes, err := db.GetCauses()
+		if err != nil {
+			return err
+		}
+		return c.Render(http.StatusOK, "base", struct {
+			Page string
+			Data interface{}
+		}{
+			Page: "index",
+			Data: causes,
+		})
+	})
+	e.GET("/signup", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "base", struct {
+			Page string
+		}{
+			Page: "signup",
+		})
+	})
+	e.GET("/login", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "base", struct {
+			Page string
+		}{
+			Page: "login",
+		})
+	})
+	e.GET("/create", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "base", struct {
+			Page string
+		}{
+			Page: "create",
+		})
+	})
+	e.GET("/cause/:name", func(c echo.Context) error {
+		cause, err := db.GetCause(c.Param("name"))
+		if err != nil {
+			return err
+		}
+		return c.Render(http.StatusOK, "base", struct {
+			Page string
+			Data interface{}
+		}{
+			Page: "send",
+			Data: cause,
+		})
+	})
 }
